93234958c29486378e36187b44c87b1f36dd6dec
Guilherme Alexandre <guilhermegrilo2009@hotmail.com>
2025-03-07 21:44:53 -0300

    Adding middlewares and database

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 import http from 'node:http'
 
+import { json } from './middlewares/json.js'
+
 const server = http.createServer(async (req, res) => {
+  const { method, url } = req
+
+  await json(req, res)
+
   return res.writeHead(404).end('Not Found')
 })
 
